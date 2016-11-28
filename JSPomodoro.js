08289bb53c335d00ed27bb87d94c1cb6e5198430
maiquel <maiquel_craash@hotmail.com>
2016-11-28 20:10:12 -0200

    Implemented initial logic and objects structures.

--- JSPomodoro.js
+++ JSPomodoro.js
@@ -5,31 +5,73 @@
 (function () {
 	"use strict";
 
-	const pomodoro_canvas = document.getElementById("counter"),
-		ctx = pomodoro_canvas.getContext('2d'),						//contexto 2D
-		circ = Math.PI * 2,											//360º
-		quart = Math.PI / 2,										//90º
-		width = pomodoro_canvas.width,
-		height = pomodoro_canvas.height;
+	var JSPomodoro = function () {
 
-	var imd = null;													//image of the canvas variable
+		const pomodoro_canvas = document.querySelector("#pomodoro .progress"),
+			ctx = pomodoro_canvas.getContext('2d'),						//contexto 2D
+			circ = Math.PI * 2,											//360º
+			quart = Math.PI / 2,										//90º
+			width = pomodoro_canvas.width,
+			height = pomodoro_canvas.height;
 
-	ctx.beginPath();
-	ctx.strokeStyle = '#99CC33';
-	ctx.lineCap = 'square';											//set the end of the path to square style
-	ctx.closePath();
-	ctx.fill();
-	ctx.lineWidth = 10;
+		var countdown = document.querySelector("#pomodoro .countdown");
 
-	imd = ctx.getImageData(0, 0, width, height);					//get an image of the canvas starting on 0,0 point and get the full canvas size
+		var imd = null;													//image of the canvas variable
 
-	var draw = function(current) {
-		ctx.putImageData(imd, 0, 0);
-		ctx.beginPath();
-		ctx.arc(width/2, height/2, 70, -(quart), ((circ) * current) - quart, false);
-		ctx.stroke();
+		imd = ctx.getImageData(0, 0, width, height);					//get an image of the canvas starting on 0,0 point and get the full canvas size
+
+		var core = {
+			time: 150000,
+			timeleft: 150000,
+			running: false,
+
+			init: function () {
+				ctx.beginPath();
+				ctx.strokeStyle = '#99CC33';
+				ctx.lineCap = 'square';											//set the end of the path to square style
+				ctx.closePath();
+				ctx.fill();
+				ctx.lineWidth = 10;
+
+				core.render(1);
+			},
+
+			render: function () {
+				var current = (core.timeleft / core.time) || 0;
+				ctx.putImageData(imd, 0, 0);
+				ctx.beginPath();
+				ctx.arc(width / 2, height / 2, 70, -(quart), (circ - (circ * current)) - quart, false);
+				ctx.stroke();
+			},
+			start: function () {
+				if (!core.running) {
+					core.running = true;
+					var run = setInterval(function () {
+						core.timeleft -= 1000;
+						if (core.running && core.timeleft > 0) {
+							render();
+							run();
+						}
+						else {
+							core.finnish();
+						}
+					}, 1000);
+				}
+			},
+
+			stop: function () {
+
+			},
+
+			finnish: function () {
+				core.running = false;
+				core.timeleft = 150000;
+			}
+		};
+		core.init();
 	};
 
-	draw(0.9);
+	JSPomodoro();
+
 
 }());
